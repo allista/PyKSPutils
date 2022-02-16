aa3c5d19f37539efc3ea28472de5a112cbcc6f32
Allis Tauri <allista@gmail.com>
2022-02-16 12:52:29 +0300

    Using setuptools instead of distutils in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
         name='PyKSPutils',
         version='1.0',
-        packages=['KSPUtils'],
         scripts=['make_mod_release', 'grep_parts', 'select_from_parts', 'update_KSP_references'],
         url='',
         license='MIT',
         author='Allis Tauri',
         author_email='allista@gmail.com',
         description='A collection of tools and utility classes for KSP modders.'
+    packages=find_packages(),
 )
