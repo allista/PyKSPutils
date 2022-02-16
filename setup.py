fb76d76fac47e11aae0dd9b8aa3a1739c03bae68
Allis Tauri <allista@gmail.com>
2022-02-16 12:54:13 +0300

    black: setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='PyKSPutils',
-        version='1.0',
-        scripts=['make_mod_release', 'grep_parts', 'select_from_parts', 'update_KSP_references'],
-        url='',
-        license='MIT',
-        author='Allis Tauri',
-        author_email='allista@gmail.com',
-        description='A collection of tools and utility classes for KSP modders.'
+    name="PyKSPutils",
+    version="1.0",
+    author="Allis Tauri",
+    author_email="allista@gmail.com",
+    description="A collection of tools and utility classes for KSP modders.",
+    license="MIT",
+    url="",
     packages=find_packages(),
     python_requires=">=3.8",
+    scripts=[
+        "grep_parts",
+        "select_from_parts",
+        "update_KSP_references",
+        "make_mod_release",
+    ],
 )
