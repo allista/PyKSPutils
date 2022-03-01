7c8dadfcb5c086a382886c8d1bc38cd71018c08c
Allis Tauri <allista@gmail.com>
2022-03-01 18:15:41 +0300

    Added YamlExtractor to load dataclasses from yaml

---
+++ KSPUtils/info_extractors/yaml_extractor.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional, Type
+
+from yaml import safe_load
+
+from KSPUtils.info_extractors.file_extractor import (
+    FileExtractor,
+    FileExtractorType,
+    StrPath,
+)
+
+
+class YamlExtractor(FileExtractor):
+    @classmethod
+    def from_file(
+        cls: Type[FileExtractorType], filename: StrPath, **kwargs: Any
+    ) -> Optional[FileExtractorType]:
+        filepath, mod_time = cls._resolve_path(filename)
+        with filepath.open("rt", encoding="utf8") as inp:
+            data = safe_load(inp) or {}
+            return cls(**data, **kwargs)
