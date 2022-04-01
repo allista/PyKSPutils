b4ae88c43c134ec743f103103e22d0bc1d52fece
Allis Tauri <allista@gmail.com>
2022-04-01 20:16:08 +0300

    SpacedockError adds response.content to its str, if available

--- KSPUtils/spacedock/errors.py
+++ KSPUtils/spacedock/errors.py
@@ -1,5 +1,15 @@
+from requests import Response
+
 from KSPUtils.exception_chain import ExceptionChain
 
 
 class SpacedockError(ExceptionChain):
     """Generic spacedock exception"""
+
+    def __str__(self):
+        res = super().__str__()
+        if self.cause:
+            response = getattr(self.cause, "response", None)
+            if isinstance(response, Response):
+                return f"{res}\n{response.content}"
+        return res
